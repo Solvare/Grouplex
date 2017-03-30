3d56d53b873f7ec71a669862c488d0b41b20553e
rajatsaxena035 <rajat.saxena035@gmail.com>
2017-03-30 18:56:46 +0530

    minor api changes

--- api/v1/index.php
+++ api/v1/index.php
@@ -11,7 +11,7 @@ require dirname(__FILE__) . '../../include/db_connect.php';
 $app = new \Slim\Slim();
 
 /*------------------------------------------------------------------------------------*/
-//	1. fetching all groups of a particular user
+//  1. fetching all groups of a particular user
 /*------------------------------------------------------------------------------------*/
  
 $app->get('/:user_id/groups', function($user_id)
@@ -30,7 +30,7 @@ $app->get('/:user_id/groups', function($user_id)
     }
     else
     {
-        $stmt = $conn->query("SELECT group_name FROM master,groups WHERE master.user_id = $user_id AND groups.group_id = master.group_id");
+        $stmt = $conn->query("SELECT group_name,admin_id,image FROM master,groups WHERE master.user_id = $user_id AND groups.group_id = master.group_id");
         if($stmt->rowCount()==0)
         {
             $response["error"] = true;
@@ -42,19 +42,22 @@ $app->get('/:user_id/groups', function($user_id)
             $response["groups"] = array();
             foreach($stmt as $row)
             {
-                array_push($response["groups"], $row["group_name"]);
+                $tmp = array();
+                $tmp["name"] = $row["group_name"];
+                $tmp["level"] = ($user_id == $row["admin_id"] ? "admin" : "member");
+                //$tmp["image"] = "http://"."$_SERVER[HTTP_HOST]"."/grouplex/api/images/".$row["image"];
+                array_push($response["groups"], $tmp);
             }
         }  
     }
     $exist = null;
     $stmt = null;
     $conn = null;
-
     echoRespnse(200, $response);
 });
 
 /*------------------------------------------------------------------------------------*/
-//	2. fetching all messages of a particular group
+//  2. fetching all messages of a particular group
 /*------------------------------------------------------------------------------------*/
 
 $app->get('/:group_id/messages', function($group_id)
@@ -97,7 +100,7 @@ $app->get('/:group_id/messages', function($group_id)
 });
 
 /*------------------------------------------------------------------------------------*/
-//	3. fetching all members of a particular group
+//  3. fetching all members of a particular group
 /*------------------------------------------------------------------------------------*/
 
 $app->get('/:group_id/members', function($group_id)
@@ -133,16 +136,19 @@ $app->get('/:group_id/members', function($group_id)
 });
 
 /*------------------------------------------------------------------------------------*/
-//	4. posting message to a particular group
+//  4. posting message to a particular group
 /*------------------------------------------------------------------------------------*/
 
-$app->post('/:group_id/:user_id/post', function($group_id,$user_id) use ($app)
+$app->post('/message', function() use ($app)
 {   
     global $app;
     $response = array();
    
     $db = new DbConnect();
     $conn = $db->connect();
+
+    $group_id = $app->request->post('group_id');
+    $user_id = $app->request->post('user_id');
 
     //check if group exists or not
     $exist_group = $conn->query("SELECT group_id FROM groups WHERE group_id = $group_id");
@@ -166,7 +172,7 @@ $app->post('/:group_id/:user_id/post', function($group_id,$user_id) use ($app)
         {
             $chat = $app->request->post('chat');
             $insrt = $conn->exec("INSERT INTO messages(group_id,chat) VALUES('$group_id','$chat')");
-			
+            
             if($insrt==1)
             {
                 $response["error"] = false;
@@ -195,7 +201,7 @@ $app->post('/:group_id/:user_id/post', function($group_id,$user_id) use ($app)
 
 $app->get('/search/:query', function($query)
 {
-	global $app;
+    global $app;
     $response = array();
    
     $db = new DbConnect();
@@ -220,25 +226,25 @@ $app->get('/search/:query', function($query)
     
     $stmt = null;
     $conn = null;
-	
+    
     echoRespnse(200, $response);
 });
 
 /*------------------------------------------------------------------------------------*/
-//	6. user registration
+//  6. user registration
 /*------------------------------------------------------------------------------------*/
 
 $app->post('/user/register',function() use ($app)
 {
-	global $app;
-	$response=array();
+    global $app;
+    $response=array();
 
-	$db = new DbConnect();
+    $db = new DbConnect();
     $conn = $db->connect();
-	
-	$email=$app->request->post('email');
-	$password=$app->request->post('password');
-	$full_name=$app->request->post('full_name');
+    
+    $email=$app->request->post('email');
+    $password=$app->request->post('password');
+    $full_name=$app->request->post('full_name');
     
     if($email == null || $password == 'gow' || $full_name == null)
     {
@@ -269,26 +275,26 @@ $app->post('/user/register',function() use ($app)
             }
         }
     }
-	
+    
     $conn = null;
-	$user_exist=null;
-	echoRespnse(200,$response);
+    $user_exist=null;
+    echoRespnse(200,$response);
 });
 
 /*------------------------------------------------------------------------------------*/
-//	7. user login
+//  7. user login
 /*------------------------------------------------------------------------------------*/
 
 $app->post('/user/login',function() use ($app)
 {
-	$response=array();
+    $response=array();
 
-	$db = new DbConnect();
+    $db = new DbConnect();
     $conn = $db->connect();
-	
+    
     $email=$app->request->post('email');
-	$password=$app->request->post('password');
-	
+    $password=$app->request->post('password');
+    
     if($email == 'gow' || $password == 'gow')
     {
         $response["error"]=true;
@@ -297,117 +303,228 @@ $app->post('/user/login',function() use ($app)
     else
     {
         $user_exist=$conn->query("select user_id,email,full_name,password as passhash from users where email='$email'");
-    	if($user_exist->rowCount()==1)
-    	{						
-    		$row = $user_exist->fetch();		
-    		$passhash = $row['passhash'];
-    		if(PassHash::check_password($passhash, $password))
-    		{
-    			$response["error"]=false;
-    			$response["message"]="user successfully logged in";
-    			$response["user_id"]=$row['user_id'];
-    			$response["email"]=$row['email'];
-    			$response["full_name"]=$row['full_name'];
-    		}
-    		else
-    		{
-    			$response["error"]=true;
-    			$response["message"]="incorrect password";	
-    		}
-    	}
-    	else
-    	{
-    		$response["error"]=true;
-    		$response["message"]="user does not exist";
-    	}
+        if($user_exist->rowCount()==1)
+        {                       
+            $row = $user_exist->fetch();        
+            $passhash = $row['passhash'];
+            if(PassHash::check_password($passhash, $password))
+            {
+                $response["error"]=false;
+                $response["message"]="user successfully logged in";
+                $response["user_id"]=$row['user_id'];
+                $response["email"]=$row['email'];
+                $response["full_name"]=$row['full_name'];
+            }
+            else
+            {
+                $response["error"]=true;
+                $response["message"]="incorrect password";  
+            }
+        }
+        else
+        {
+            $response["error"]=true;
+            $response["message"]="user does not exist";
+        }
     }
     $conn = null;
     $user_exist = null;
 
-	echoRespnse(200,$response);
+    echoRespnse(200,$response);
 });
 
 /*------------------------------------------------------------------------------------*/
-//	8. creating a new group
+//  8. creating a new group
 /*------------------------------------------------------------------------------------*/
 
-$app->post('/newgroup/:user_id',function($user_id) use ($app)
+$app->post('/creategroup',function() use ($app)
 {
-	$response=array();
-	
-	$db = new DbConnect();
+    $response=array();
+    
+    $db = new DbConnect();
     $conn = $db->connect();
-	
-	$user_exist=$conn->query("select user_id from users where user_id=$user_id");
-	if($user_exist->rowCount()>0){
-		$group_name=$app->request->post('group_name');
-		
-		$query=$conn->query("select group_id from groups where group_name='$group_name'");
-		if($query->rowCount()>0){
-			$response["error"]=true;
-			$response["message"]="group name already exist";
-		}else{
-			if($conn->query("Insert into groups(group_name,admin_id) VALUES('$group_name','$user_id')")==TRUE){			
-				$response["error"]=false;
-				$response["message"]="new group created";
-			}else{
-				$response["error"]=true;
-				$response["message"]="error occurred while creating group";
-			}
-		}
-	}
-	else{
-		$response["error"]=true;
-		$response["message"]="user does not exist";
-	}
-	$conn=null;
-	echoRespnse(200,$response);
+    
+    $group_name=$app->request->post('group_name');
+    $user_id=$app->request->post('user_id');
+
+    $user_exist=$conn->query("select user_id from users where user_id=$user_id");
+    if($user_exist->rowCount()>0){
+        
+        $query=$conn->query("select group_id from groups where group_name='$group_name'");
+        if($query->rowCount()>0){
+            $response["error"]=true;
+            $response["message"]="group name already exist";
+        }else{
+            $group_table = $conn->query("Insert into groups(group_name,admin_id) VALUES('$group_name','$user_id')");
+            $group_id = $conn->lastInsertId();
+            $master_table = $conn->query("Insert into master(group_id,user_id) VALUES('$group_id','$user_id')");
+            if($group_table == TRUE && $master_table == TRUE){         
+                $response["error"]=false;
+                $response["message"]="new group created";
+            }else{
+                $response["error"]=true;
+                $response["message"]="error occurred while creating group";
+            }
+        }
+    }
+    else{
+        $response["error"]=true;
+        $response["message"]="user does not exist";
+    }
+    $conn=null;
+    $user_exist=null;
+    $query=null;
+    $group_table=null;
+    $master_table=null;
+    echoRespnse(200,$response);
 });
 
 /*------------------------------------------------------------------------------------*/
-//	9. joining a group
+//  9. joining a group
 /*------------------------------------------------------------------------------------*/
 
-$app->post('/joingroup/:user_id',function($user_id) use ($app){
-	$response=array();
+$app->post('/joingroup',function() use ($app)
+{
+    $response=array();
 
-	$db = new DbConnect();
+    $db = new DbConnect();
     $conn = $db->connect();
-	
-	$user_exist=$conn->query("select user_id from users where user_id=$user_id");
-	if($user_exist->rowCount()>0){
-		$group_name=$app->request->get('group_name');
-		$query=$conn->query("select * from groups where group_name='$group_name'");
-		
-		if($query->rowCount()>0){
-			while($param=$query->fetch(PDO::FETCH_OBJ))
-			{
-				$group_id=$param->group_id;
-			}
-			$userjoin=$conn->query("select id from master where group_id=$group_id AND user_id=$user_id");
-			if($userjoin->rowCount()>0){
-				$response["error"]=true;
-				$response["message"]="user already joined the group";
-			}else{
-				if($conn->query("Insert into master(group_id,user_id) VALUES('$group_id','$user_id')")==TRUE){
-					$response["error"]=false;
-					$response["message"]="group joined";
-				}
-				else{
-					$response["error"]=true;
-					$response["message"]="error occurred while joining group";
-				}
-			}
-		}else{
-			$response["error"]=true;
-			$response["message"]="group does not exist";
-		}
-	}else{
-		$response["error"]=true;
-		$response["message"]="user does not exist";
-	}
-	$conn=null;
-	echoRespnse(200,$response);
+    
+    $group_id=$app->request->post('group_id');
+    $user_id=$app->request->post('user_id');
+
+    $user_exist=$conn->query("select user_id from users where user_id=$user_id");
+    if($user_exist->rowCount()>0){
+        $query=$conn->query("select * from groups where group_id=$group_id");
+        if($query->rowCount()>0){
+            while($param=$query->fetch(PDO::FETCH_OBJ))
+            {
+                $group_id=$param->group_id;
+            }
+            $userjoin=$conn->query("select id from master where group_id=$group_id AND user_id=$user_id");
+            if($userjoin->rowCount()>0){
+                $response["error"]=true;
+                $response["message"]="user already joined the group";
+            }else{
+                if($conn->query("Insert into master(group_id,user_id) VALUES('$group_id','$user_id')")==TRUE){
+                    $response["error"]=false;
+                    $response["message"]="group joined";
+                }
+                else{
+                    $response["error"]=true;
+                    $response["message"]="error occurred while joining group";
+                }
+            }
+        }else{
+            $response["error"]=true;
+            $response["message"]="group does not exist";
+        }
+    }else{
+        $response["error"]=true;
+        $response["message"]="user does not exist";
+    }
+    $conn=null;
+    $user_exist=null;
+    $query=null;
+    $userjoin=null;
+    echoRespnse(200,$response);
+});
+
+/*------------------------------------------------------------------------------------*/
+//  10. Upload Group Image
+/*------------------------------------------------------------------------------------*/
+
+$app->post('/image', function() use ($app)
+{   
+    /*
+    echo "here";
+    global $app;
+    $response = array();
+   
+    $db = new DbConnect();
+    $conn = $db->connect();
+
+    $group_id = $app->request->post('group_id');
+    $user_id = $app->request->post('user_id');
+
+    //check if group exists or not
+    $exist_group = $conn->query("SELECT group_id FROM groups WHERE group_id = $group_id");
+    $exist_user = $conn->query("SELECT user_id FROM users WHERE user_id = $user_id");
+    
+    if($exist_group->rowCount()==0 || $exist_user->rowCount()==0)
+    {
+        $response["error"] = true;
+        $response["message"] = "The group or user does not exists";
+    }
+    else
+    {
+        $stmt = $conn->query("SELECT admin_id FROM groups WHERE admin_id = $user_id AND group_id=$group_id");
+        if($stmt->rowCount()==0)
+        {
+            $response["error"] = true;
+            $response["message"] = "The user is not authorised to upload image of this group";
+        }
+        else if($stmt->rowCount()==1)
+        {
+
+            echo "here";
+            
+                $target_dir = "http://"."$_SERVER[HTTP_HOST]"."/grouplex/api/images/";
+                $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+                echo $target_file;
+                $uploadOk = 1;
+                $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+            
+                // Check file size
+                if ($_FILES["fileToUpload"]["size"] > 500000) {
+                    $response["message1"] = "Sorry, your file is too large";
+                    $uploadOk = 0;
+                }
+                
+                // Allow certain file formats
+                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif" ) {
+                    $response["message2"] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed";
+                    $uploadOk = 0;
+                }
+                // Check if $uploadOk is set to 0 by an error
+                if ($uploadOk == 0) {
+                    $response["error"] = true;
+                // if everything is ok, try to upload file
+                } 
+                else {
+                    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                        $response["error"] = false;
+                        $response["message3"] = "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded."
+                    } else {
+                        $response["error"] = true;
+                        $response["message3"] = "Sorry, there was an error uploading your file";
+                    }
+                }
+            
+            $insrt = $conn->exec("INSERT INTO groups(image) VALUES('image.jpg')");
+            
+            if($insrt==1)
+            {
+                $response["error"] = false;
+                $response["chat"] = $chat;
+            }
+            else
+            {
+                $response["error"] = true;
+                $response["message"] = "Insertion Failed";
+            }
+            
+        }
+    }
+    $exist_group = null;
+    $exist_user = null;
+    $insrt = null;
+    $stmt = null;
+    $conn = null;
+
+    echoRespnse(200, $response);
+    */
 });
 
 
@@ -429,4 +546,4 @@ function echoRespnse($status_code, $response) {
 
 $app->run();
 
-?>
+?>  
