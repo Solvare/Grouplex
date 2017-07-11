163770db085fedbe6126e90c359e47d788935fa6
rishabh279 <rishabhahuja279@gmail.com>
2017-07-12 01:00:35 +0530

    OnClickLstener added for RecyclerView

---
+++ android/grouplex/app/src/main/java/com/example/solvare/grouplex/custom/MessageDetails.java
@@ -0,0 +1,15 @@
+package com.example.solvare.grouplex.custom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.solvare.grouplex.R;
+
+public class MessageDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
--- android/grouplex/app/src/main/java/com/example/solvare/grouplex/custom/MyGroupsAdapter.java
+++ android/grouplex/app/src/main/java/com/example/solvare/grouplex/custom/MyGroupsAdapter.java
@@ -1,8 +1,10 @@
 package com.example.solvare.grouplex.custom;
  
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,10 +15,11 @@ import com.example.solvare.grouplex.R;
 
 public class MyGroupsAdapter extends RecyclerView.Adapter<MyGroupsAdapter.MyViewHolder> {
 
-    List<MyGroups> mData;
+    ArrayList<MyGroups> mData;
     private Context context;
+    
 
-    public MyGroupsAdapter(Context context, List<MyGroups> data) {
+    public MyGroupsAdapter(Context context, ArrayList<MyGroups> data) {
         super();
         this.context = context;
         this.mData = data;
@@ -24,7 +27,7 @@ public class MyGroupsAdapter extends RecyclerView.Adapter<MyGroupsAdapter.MyView
     @Override
     public MyGroupsAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_my_groups, parent, false);
-        MyViewHolder holder = new MyViewHolder(view);
+        MyViewHolder holder = new MyViewHolder(view,context,mData);
         return holder;
     }
     @Override
@@ -38,14 +41,18 @@ public class MyGroupsAdapter extends RecyclerView.Adapter<MyGroupsAdapter.MyView
         return mData.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder {
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView groupname,level,num_members;
         int position;
         MyGroups groups;
-
-        public MyViewHolder(View itemView) {
+        Context ctx;
+        List<MyGroups> mData = new ArrayList<>();
+        public MyViewHolder(View itemView, Context ctx,ArrayList<MyGroups> mData){
             super(itemView);
+            this.ctx=ctx;
+            this.mData=mData;
+            itemView.setOnClickListener(this);
             groupname=(TextView)itemView.findViewById(R.id.group_name);
             level=(TextView)itemView.findViewById(R.id.your_level);
             num_members=(TextView)itemView.findViewById(R.id.total_members);
@@ -57,6 +64,14 @@ public class MyGroupsAdapter extends RecyclerView.Adapter<MyGroupsAdapter.MyView
             this.num_members.setText(groups.getNumMembers().get(position));
             this.position=position;
             this.groups=groups;
+        }
+        @Override
+        public void onClick(View v){
+            int position =getAdapterPosition();
+            MyGroups groups=this.mData.get(position);
+            Intent intent = new Intent(this.ctx,MessageDetails.class);
+            this.ctx.startActivity(intent);
+
         }
     }
 
